e2f1613c4531a0f72eec4d129ea2472b21ae3033
Michael <Magawrych85@gmail.com>
2024-11-06 14:23:58 -0500

    updated

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,3 +1,46 @@
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
 export default function Price () {
-    return <h1>This is the Price Component</h1>;
+  
+    const { symbol } = useParams();
+
+    const [coin, setCoin] = useState(null);
+
+    const apiKey='5371F778-AE84-4B7C-8439-4D8E6CEEA718'
+    const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+
+    async function getCoin() {
+      try {
+        let res = await fetch(url);
+        let data = await res.json();
+
+        setCoin(data);
+
+      } catch (error) {
+        console.error(err)
+      }
+    }
+
+    useEffect(() => {
+      getCoin();
+      
+    }, []);
+
+    const loading = () => {
+      return <h1>Loading...</h1>;
+    };
+
+    const loaded = () => {
+      return (
+        <div>
+          <h1>
+            {coin.asset_id_base}/{coin.asset_id_quote}
+          </h1>
+          <h2>{coin.rate}</h2>
+        </div>
+      );
+    };
+  
+    return coin && coin.rate ? loaded() : loading();
   }
